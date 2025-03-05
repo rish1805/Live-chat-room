9aff3a83b7c0a39dceb5d9c572c3214145f033d4
rish1805 <rishikesh.m.bhatt@gmail.com>
2025-03-05 12:45:44 +0100

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,56 @@
+import ChatUI from './display.js';
+import Chatroom from './chat.js';
+
+// dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
+// update the username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  newNameForm.reset();
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => {
+  if (e.target.tagName === 'BUTTON') {
+    const buttons = rooms.querySelectorAll('button');
+    buttons.forEach(button => {
+      button.classList.remove('active');
+      button.style.color = '';  
+    });
+
+    e.target.classList.add('active');
+    e.target.style.color = 'black';
+
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
+
+// check local storage for name
+const username = localStorage.username ? localStorage.username : 'Anonymous';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+// get chats & render
+chatroom.getChats(data => chatUI.render(data));
